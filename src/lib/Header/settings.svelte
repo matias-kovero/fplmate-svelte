28e6c5a9b78ce6517e332a8b69efa6b4596a4dcc
matias-kovero <matias.kovero@gmail.com>
2021-06-05 23:54:10 +0300

    custom layout for settings sub folders

---
+++ src/lib/Header/settings.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import logo from './logo.png';
+  import BackIcon from 'carbon-icons-svelte/lib/ArrowLeft32/ArrowLeft32.svelte';
+
+  $: subPath = $page.path !== '/settings';
+</script>
+
+<header>
+  {#if subPath}
+  <div class="corner">
+    <a sveltekit:prefetch href="/settings">
+			<BackIcon aria-labelledby="Back" />
+		</a>
+	</div>
+  {:else}
+  <div></div>
+  {/if}
+
+	<div class="center">
+		<a href="/">
+			<img src={logo} alt="FPLMate" />
+		</a>
+	</div>
+</header>
+
+<style>
+  header {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		place-items: center;
+		/*justify-content: space-between; */
+		background-color: var(--pure-white);
+	}
+	.center, .corner {
+		width: 3em;
+		height: 3em;
+	}
+  .center a, .corner a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+  .center img {
+		width: 2em;
+		height: 2em;
+		object-fit: contain;
+	}
+  .corner a {
+    color: var(--darkpurple);
+  }
+</style>
---
+++ src/routes/settings/index.svelte
@@ -0,0 +1,37 @@
+<script context="module">  
+  export const prerender = true;
+</script>
+<script>
+  // Scripts needed here?
+  // We need percistent store for user settings
+  // Should we trust localStorage??
+</script>
+
+<svelte:head>
+  <title>Settings</title>
+</svelte:head>
+
+<div class="settings">
+  <div class="block">
+    <p><a sveltekit:prefetch href="/settings/1">Option 1</a></p>
+  </div>
+  <div class="block">
+    <p><a sveltekit:prefetch href="/settings/2">Option 2</a></p>
+  </div>
+  <div class="block">
+    <p>Option 3</p>
+  </div>
+</div>
+
+<style>
+  .settings {
+    padding-top: 10%;
+    display: grid;
+    gap: 3em;
+  }
+  .block {
+    padding: .5em;
+    border-radius: .5em;
+    background-color: var(--pure-white);
+  }
+</style>
